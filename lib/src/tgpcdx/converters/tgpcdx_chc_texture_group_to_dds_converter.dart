6eae287c7204dbd01e6c23ccb166a159d0be6432
Thomas Clark <tnc1997@outlook.com>
2024-09-08 12:44:54 +0100

    feat: add tgpcdx to dds converter sink

--- lib/src/tgpcdx/converters/tgpcdx_chc_texture_group_to_dds_converter.dart
+++ lib/src/tgpcdx/converters/tgpcdx_chc_texture_group_to_dds_converter.dart
@@ -30,258 +30,294 @@ class TgpcdxChcTextureGroupToDdsConverter
     );
   }
 
-  int? _getABitMask(
-    String format,
+  @override
+  Sink<TgpcdxChcTextureGroup> startChunkedConversion(
+    Sink<Dds> sink,
   ) {
-    switch (format) {
-      case TgpcdxTextureFormats.col888:
-      case TgpcdxTextureFormats.compressed:
-      case TgpcdxTextureFormats.compressedExplicitAlpha:
-      case TgpcdxTextureFormats.compressedInterpolatedAlpha:
-        return null;
-      case TgpcdxTextureFormats.cola8888:
-        return 0xff000000;
-      default:
-        throw Exception('Invalid texture format');
-    }
+    return _TgpcdxChcTextureGroupToDdsConverterSink(
+      sink: sink,
+    );
   }
+}
 
-  int? _getBBitMask(
-    String format,
+class _TgpcdxChcTextureGroupToDdsConverterSink
+    implements Sink<TgpcdxChcTextureGroup> {
+  final Sink<Dds> _sink;
+
+  _TgpcdxChcTextureGroupToDdsConverterSink({
+    required Sink<Dds> sink,
+  }) : _sink = sink;
+
+  @override
+  void add(
+    TgpcdxChcTextureGroup data,
   ) {
-    switch (format) {
-      case TgpcdxTextureFormats.col888:
-      case TgpcdxTextureFormats.cola8888:
-        return 0xff;
-      case TgpcdxTextureFormats.compressed:
-      case TgpcdxTextureFormats.compressedExplicitAlpha:
-      case TgpcdxTextureFormats.compressedInterpolatedAlpha:
-        return null;
-      default:
-        throw Exception('Invalid texture format');
-    }
-  }
-
-  int _getCaps(
-    List<TgpcdxChcImageDx> chcImageDxs,
-  ) {
-    var caps = DdsCaps.texture;
-
-    if (chcImageDxs.length > 1) {
-      caps |= DdsCaps.complex | DdsCaps.mipMap;
-    }
-
-    return caps;
-  }
-
-  int _getCompressionBlockSize(
-    String format,
-  ) {
-    // https://learn.microsoft.com/en-us/windows/win32/direct3ddds/dds-file-layout-for-textures
-    switch (format) {
-      case TgpcdxTextureFormats.compressed:
-        return 8;
-      case TgpcdxTextureFormats.compressedExplicitAlpha:
-      case TgpcdxTextureFormats.compressedInterpolatedAlpha:
-        return 16;
-      default:
-        throw Exception('Invalid texture format');
-    }
-  }
-
-  Iterable<int> _getData(
-    List<TgpcdxChcImageDx> chcImageDxs,
-  ) sync* {
-    for (final chcImageDx in chcImageDxs.sorted(
-      (a, b) {
-        if (a.width >= a.height) {
-          return a.width - b.width;
-        } else {
-          return a.height - b.height;
-        }
-      },
-    ).reversed) {
-      yield* chcImageDx.blob;
-    }
-  }
-
-  int? _getFourCc(
-    String format,
-  ) {
-    switch (format) {
-      case TgpcdxTextureFormats.col888:
-      case TgpcdxTextureFormats.cola8888:
-        return null;
-      case TgpcdxTextureFormats.compressed:
-        return DdsD3dFormats.dxt1;
-      case TgpcdxTextureFormats.compressedExplicitAlpha:
-        return DdsD3dFormats.dxt3;
-      case TgpcdxTextureFormats.compressedInterpolatedAlpha:
-        return DdsD3dFormats.dxt5;
-      default:
-        throw Exception('Invalid texture format');
-    }
-  }
-
-  int? _getGBitMask(
-    String format,
-  ) {
-    switch (format) {
-      case TgpcdxTextureFormats.col888:
-      case TgpcdxTextureFormats.cola8888:
-        return 0xff00;
-      case TgpcdxTextureFormats.compressed:
-      case TgpcdxTextureFormats.compressedExplicitAlpha:
-      case TgpcdxTextureFormats.compressedInterpolatedAlpha:
-        return null;
-      default:
-        throw Exception('Invalid texture format');
-    }
-  }
-
-  DdsHeader _getHeader(
-    TgpcdxChcTextureGroup chcTextureGroup,
-  ) {
-    return DdsHeader(
-      flags: _getHeaderFlags(
-          chcTextureGroup.texture.chcTexture.data.chcTextureData.mip.chcImageDxs
-              .first.format,
-          chcTextureGroup
-              .texture.chcTexture.data.chcTextureData.mip.chcImageDxs),
-      height: chcTextureGroup.texture.chcTexture.height,
-      width: chcTextureGroup.texture.chcTexture.width,
-      pitchOrLinearSize: _getPitchOrLinearSize(
-          chcTextureGroup.texture.chcTexture.data.chcTextureData.mip.chcImageDxs
-              .first.format,
-          chcTextureGroup.texture.chcTexture.width),
-      depth: null,
-      mipMapCount: chcTextureGroup.texture.chcTexture.data.chcTextureData.mip
-                  .chcImageDxs.length >
-              1
-          ? chcTextureGroup
-              .texture.chcTexture.data.chcTextureData.mip.chcImageDxs.length
-          : null,
-      pixelFormat: _getPixelFormat(chcTextureGroup),
-      caps: _getCaps(chcTextureGroup
-          .texture.chcTexture.data.chcTextureData.mip.chcImageDxs),
+    _sink.add(
+      Dds(
+        header: _getHeader(data),
+        data: _getData(
+                data.texture.chcTexture.data.chcTextureData.mip.chcImageDxs)
+            .toList(),
+      ),
     );
   }
 
-  int _getHeaderFlags(
-    String format,
-    List<TgpcdxChcImageDx> chcImageDxs,
-  ) {
-    var flags = DdsHeaderFlags.caps |
-        DdsHeaderFlags.height |
-        DdsHeaderFlags.width |
-        DdsHeaderFlags.pixelFormat;
+  @override
+  void close() {
+    _sink.close();
+  }
+}
 
-    switch (format) {
-      case TgpcdxTextureFormats.col888:
-      case TgpcdxTextureFormats.cola8888:
-        flags |= DdsHeaderFlags.pitch;
-        break;
-      case TgpcdxTextureFormats.compressed:
-      case TgpcdxTextureFormats.compressedExplicitAlpha:
-      case TgpcdxTextureFormats.compressedInterpolatedAlpha:
-        flags |= DdsHeaderFlags.linearSize;
-        break;
-      default:
-        throw Exception('Invalid texture format');
-    }
+int? _getABitMask(
+  String format,
+) {
+  switch (format) {
+    case TgpcdxTextureFormats.col888:
+    case TgpcdxTextureFormats.compressed:
+    case TgpcdxTextureFormats.compressedExplicitAlpha:
+    case TgpcdxTextureFormats.compressedInterpolatedAlpha:
+      return null;
+    case TgpcdxTextureFormats.cola8888:
+      return 0xff000000;
+    default:
+      throw Exception('Invalid texture format');
+  }
+}
 
-    if (chcImageDxs.length > 1) {
-      flags |= DdsHeaderFlags.mipMapCount;
-    }
+int? _getBBitMask(
+  String format,
+) {
+  switch (format) {
+    case TgpcdxTextureFormats.col888:
+    case TgpcdxTextureFormats.cola8888:
+      return 0xff;
+    case TgpcdxTextureFormats.compressed:
+    case TgpcdxTextureFormats.compressedExplicitAlpha:
+    case TgpcdxTextureFormats.compressedInterpolatedAlpha:
+      return null;
+    default:
+      throw Exception('Invalid texture format');
+  }
+}
 
-    return flags;
+int _getCaps(
+  List<TgpcdxChcImageDx> chcImageDxs,
+) {
+  var caps = DdsCaps.texture;
+
+  if (chcImageDxs.length > 1) {
+    caps |= DdsCaps.complex | DdsCaps.mipMap;
   }
 
-  int _getPitchOrLinearSize(
-    String format,
-    int width,
-  ) {
-    // https://learn.microsoft.com/en-us/windows/win32/direct3ddds/dx-graphics-dds-pguide
-    switch (format) {
-      case TgpcdxTextureFormats.col888:
-      case TgpcdxTextureFormats.cola8888:
-        return (width * 32 + 7) ~/ 8;
-      case TgpcdxTextureFormats.compressed:
-      case TgpcdxTextureFormats.compressedExplicitAlpha:
-      case TgpcdxTextureFormats.compressedInterpolatedAlpha:
-        return max(1, ((width + 3) ~/ 4)) * _getCompressionBlockSize(format);
-      default:
-        throw Exception('Invalid texture format');
-    }
+  return caps;
+}
+
+int _getCompressionBlockSize(
+  String format,
+) {
+  // https://learn.microsoft.com/en-us/windows/win32/direct3ddds/dds-file-layout-for-textures
+  switch (format) {
+    case TgpcdxTextureFormats.compressed:
+      return 8;
+    case TgpcdxTextureFormats.compressedExplicitAlpha:
+    case TgpcdxTextureFormats.compressedInterpolatedAlpha:
+      return 16;
+    default:
+      throw Exception('Invalid texture format');
+  }
+}
+
+Iterable<int> _getData(
+  List<TgpcdxChcImageDx> chcImageDxs,
+) sync* {
+  for (final chcImageDx in chcImageDxs.sorted(
+    (a, b) {
+      if (a.width >= a.height) {
+        return a.width - b.width;
+      } else {
+        return a.height - b.height;
+      }
+    },
+  ).reversed) {
+    yield* chcImageDx.blob;
+  }
+}
+
+int? _getFourCc(
+  String format,
+) {
+  switch (format) {
+    case TgpcdxTextureFormats.col888:
+    case TgpcdxTextureFormats.cola8888:
+      return null;
+    case TgpcdxTextureFormats.compressed:
+      return DdsD3dFormats.dxt1;
+    case TgpcdxTextureFormats.compressedExplicitAlpha:
+      return DdsD3dFormats.dxt3;
+    case TgpcdxTextureFormats.compressedInterpolatedAlpha:
+      return DdsD3dFormats.dxt5;
+    default:
+      throw Exception('Invalid texture format');
+  }
+}
+
+int? _getGBitMask(
+  String format,
+) {
+  switch (format) {
+    case TgpcdxTextureFormats.col888:
+    case TgpcdxTextureFormats.cola8888:
+      return 0xff00;
+    case TgpcdxTextureFormats.compressed:
+    case TgpcdxTextureFormats.compressedExplicitAlpha:
+    case TgpcdxTextureFormats.compressedInterpolatedAlpha:
+      return null;
+    default:
+      throw Exception('Invalid texture format');
+  }
+}
+
+DdsHeader _getHeader(
+  TgpcdxChcTextureGroup chcTextureGroup,
+) {
+  return DdsHeader(
+    flags: _getHeaderFlags(
+        chcTextureGroup.texture.chcTexture.data.chcTextureData.mip.chcImageDxs
+            .first.format,
+        chcTextureGroup.texture.chcTexture.data.chcTextureData.mip.chcImageDxs),
+    height: chcTextureGroup.texture.chcTexture.height,
+    width: chcTextureGroup.texture.chcTexture.width,
+    pitchOrLinearSize: _getPitchOrLinearSize(
+        chcTextureGroup.texture.chcTexture.data.chcTextureData.mip.chcImageDxs
+            .first.format,
+        chcTextureGroup.texture.chcTexture.width),
+    depth: null,
+    mipMapCount: chcTextureGroup
+                .texture.chcTexture.data.chcTextureData.mip.chcImageDxs.length >
+            1
+        ? chcTextureGroup
+            .texture.chcTexture.data.chcTextureData.mip.chcImageDxs.length
+        : null,
+    pixelFormat: _getPixelFormat(chcTextureGroup),
+    caps: _getCaps(
+        chcTextureGroup.texture.chcTexture.data.chcTextureData.mip.chcImageDxs),
+  );
+}
+
+int _getHeaderFlags(
+  String format,
+  List<TgpcdxChcImageDx> chcImageDxs,
+) {
+  var flags = DdsHeaderFlags.caps |
+      DdsHeaderFlags.height |
+      DdsHeaderFlags.width |
+      DdsHeaderFlags.pixelFormat;
+
+  switch (format) {
+    case TgpcdxTextureFormats.col888:
+    case TgpcdxTextureFormats.cola8888:
+      flags |= DdsHeaderFlags.pitch;
+      break;
+    case TgpcdxTextureFormats.compressed:
+    case TgpcdxTextureFormats.compressedExplicitAlpha:
+    case TgpcdxTextureFormats.compressedInterpolatedAlpha:
+      flags |= DdsHeaderFlags.linearSize;
+      break;
+    default:
+      throw Exception('Invalid texture format');
   }
 
-  DdsPixelFormat _getPixelFormat(
-    TgpcdxChcTextureGroup chcTextureGroup,
-  ) {
-    return DdsPixelFormat(
-      flags: _getPixelFormatFlags(chcTextureGroup
-          .texture.chcTexture.data.chcTextureData.mip.chcImageDxs.first.format),
-      fourCc: _getFourCc(chcTextureGroup
-          .texture.chcTexture.data.chcTextureData.mip.chcImageDxs.first.format),
-      rgbBitCount: _getRgbBitCount(chcTextureGroup
-          .texture.chcTexture.data.chcTextureData.mip.chcImageDxs.first.format),
-      rBitMask: _getRBitMask(chcTextureGroup
-          .texture.chcTexture.data.chcTextureData.mip.chcImageDxs.first.format),
-      gBitMask: _getGBitMask(chcTextureGroup
-          .texture.chcTexture.data.chcTextureData.mip.chcImageDxs.first.format),
-      bBitMask: _getBBitMask(chcTextureGroup
-          .texture.chcTexture.data.chcTextureData.mip.chcImageDxs.first.format),
-      aBitMask: _getABitMask(chcTextureGroup
-          .texture.chcTexture.data.chcTextureData.mip.chcImageDxs.first.format),
-    );
+  if (chcImageDxs.length > 1) {
+    flags |= DdsHeaderFlags.mipMapCount;
   }
 
-  int _getPixelFormatFlags(
-    String format,
-  ) {
-    switch (format) {
-      case TgpcdxTextureFormats.col888:
-        return DdsPixelFormatFlags.rgb;
-      case TgpcdxTextureFormats.cola8888:
-        return DdsPixelFormatFlags.alphaPixels | DdsPixelFormatFlags.rgb;
-      case TgpcdxTextureFormats.compressed:
-      case TgpcdxTextureFormats.compressedExplicitAlpha:
-      case TgpcdxTextureFormats.compressedInterpolatedAlpha:
-        return DdsPixelFormatFlags.fourCc;
-      default:
-        throw Exception('Invalid texture format');
-    }
-  }
+  return flags;
+}
 
-  int? _getRBitMask(
-    String format,
-  ) {
-    switch (format) {
-      case TgpcdxTextureFormats.col888:
-      case TgpcdxTextureFormats.cola8888:
-        return 0xff0000;
-      case TgpcdxTextureFormats.compressed:
-      case TgpcdxTextureFormats.compressedExplicitAlpha:
-      case TgpcdxTextureFormats.compressedInterpolatedAlpha:
-        return null;
-      default:
-        throw Exception('Invalid texture format');
-    }
+int _getPitchOrLinearSize(
+  String format,
+  int width,
+) {
+  // https://learn.microsoft.com/en-us/windows/win32/direct3ddds/dx-graphics-dds-pguide
+  switch (format) {
+    case TgpcdxTextureFormats.col888:
+    case TgpcdxTextureFormats.cola8888:
+      return (width * 32 + 7) ~/ 8;
+    case TgpcdxTextureFormats.compressed:
+    case TgpcdxTextureFormats.compressedExplicitAlpha:
+    case TgpcdxTextureFormats.compressedInterpolatedAlpha:
+      return max(1, ((width + 3) ~/ 4)) * _getCompressionBlockSize(format);
+    default:
+      throw Exception('Invalid texture format');
   }
+}
 
-  int? _getRgbBitCount(
-    String format,
-  ) {
-    switch (format) {
-      case TgpcdxTextureFormats.col888:
-      case TgpcdxTextureFormats.cola8888:
-        return 32;
-      case TgpcdxTextureFormats.compressed:
-      case TgpcdxTextureFormats.compressedExplicitAlpha:
-      case TgpcdxTextureFormats.compressedInterpolatedAlpha:
-        return null;
-      default:
-        throw Exception('Invalid texture format');
-    }
+DdsPixelFormat _getPixelFormat(
+  TgpcdxChcTextureGroup chcTextureGroup,
+) {
+  return DdsPixelFormat(
+    flags: _getPixelFormatFlags(chcTextureGroup
+        .texture.chcTexture.data.chcTextureData.mip.chcImageDxs.first.format),
+    fourCc: _getFourCc(chcTextureGroup
+        .texture.chcTexture.data.chcTextureData.mip.chcImageDxs.first.format),
+    rgbBitCount: _getRgbBitCount(chcTextureGroup
+        .texture.chcTexture.data.chcTextureData.mip.chcImageDxs.first.format),
+    rBitMask: _getRBitMask(chcTextureGroup
+        .texture.chcTexture.data.chcTextureData.mip.chcImageDxs.first.format),
+    gBitMask: _getGBitMask(chcTextureGroup
+        .texture.chcTexture.data.chcTextureData.mip.chcImageDxs.first.format),
+    bBitMask: _getBBitMask(chcTextureGroup
+        .texture.chcTexture.data.chcTextureData.mip.chcImageDxs.first.format),
+    aBitMask: _getABitMask(chcTextureGroup
+        .texture.chcTexture.data.chcTextureData.mip.chcImageDxs.first.format),
+  );
+}
+
+int _getPixelFormatFlags(
+  String format,
+) {
+  switch (format) {
+    case TgpcdxTextureFormats.col888:
+      return DdsPixelFormatFlags.rgb;
+    case TgpcdxTextureFormats.cola8888:
+      return DdsPixelFormatFlags.alphaPixels | DdsPixelFormatFlags.rgb;
+    case TgpcdxTextureFormats.compressed:
+    case TgpcdxTextureFormats.compressedExplicitAlpha:
+    case TgpcdxTextureFormats.compressedInterpolatedAlpha:
+      return DdsPixelFormatFlags.fourCc;
+    default:
+      throw Exception('Invalid texture format');
+  }
+}
+
+int? _getRBitMask(
+  String format,
+) {
+  switch (format) {
+    case TgpcdxTextureFormats.col888:
+    case TgpcdxTextureFormats.cola8888:
+      return 0xff0000;
+    case TgpcdxTextureFormats.compressed:
+    case TgpcdxTextureFormats.compressedExplicitAlpha:
+    case TgpcdxTextureFormats.compressedInterpolatedAlpha:
+      return null;
+    default:
+      throw Exception('Invalid texture format');
+  }
+}
+
+int? _getRgbBitCount(
+  String format,
+) {
+  switch (format) {
+    case TgpcdxTextureFormats.col888:
+    case TgpcdxTextureFormats.cola8888:
+      return 32;
+    case TgpcdxTextureFormats.compressed:
+    case TgpcdxTextureFormats.compressedExplicitAlpha:
+    case TgpcdxTextureFormats.compressedInterpolatedAlpha:
+      return null;
+    default:
+      throw Exception('Invalid texture format');
   }
 }
