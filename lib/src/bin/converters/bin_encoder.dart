4d8f3febd649e5919513cdad221a5b0adbb03e59
Thomas Clark <tnc1997@outlook.com>
2024-09-22 08:04:08 +0100

    feat: add bin encoder sink

--- lib/src/bin/converters/bin_encoder.dart
+++ lib/src/bin/converters/bin_encoder.dart
@@ -8,11 +8,12 @@ import '../models/bin_closing_element.dart';
 import '../models/bin_element.dart';
 import '../models/bin_opening_element.dart';
 import '../writers/bin_byte_writer.dart';
+import '../writers/bin_chunked_byte_writer.dart';
+
+const _prelude = [0x53, 0x45, 0x52, 0x5a, 0x00, 0x00, 0x01, 0x00];
 
 /// A converter that encodes an [XmlDocument] to bytes.
 class BinEncoder extends Converter<XmlDocument, List<int>> {
-  static const _prelude = [0x53, 0x45, 0x52, 0x5a, 0x00, 0x00, 0x01, 0x00];
-
   const BinEncoder();
 
   @override
@@ -21,7 +22,7 @@ class BinEncoder extends Converter<XmlDocument, List<int>> {
   ) {
     final sink = ByteAccumulatorSink();
 
-    final writer = BinByteWriter(sink);
+    final writer = BinChunkedByteWriter(sink);
 
     writer.writeBytes(_prelude);
 
@@ -30,6 +31,41 @@ class BinEncoder extends Converter<XmlDocument, List<int>> {
     sink.close();
 
     return sink.bytes;
+  }
+
+  @override
+  Sink<XmlDocument> startChunkedConversion(
+    Sink<List<int>> sink,
+  ) {
+    return _BinEncoderSink(sink);
+  }
+}
+
+class _BinEncoderSink implements Sink<XmlDocument> {
+  final Sink<List<int>> _sink;
+
+  _BinEncoderSink(this._sink);
+
+  @override
+  void add(
+    XmlDocument data,
+  ) {
+    final sink = ByteAccumulatorSink();
+
+    final writer = BinChunkedByteWriter(sink);
+
+    writer.writeBytes(_prelude);
+
+    writer.writeXmlElement(data.rootElement);
+
+    sink.close();
+
+    _sink.add(sink.bytes);
+  }
+
+  @override
+  void close() {
+    _sink.close();
   }
 }
 
